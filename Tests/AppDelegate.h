5702f97b04fdab28791fb6d09b3d8155be788c7d
Constantine Mureev <mureev@gmail.com>
2014-01-30 10:09:03 +0400

    Add test project for travis

---
+++ Tests/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Tests
+//
+//  Created by Constantine Mureev on 30/01/14.
+//  Copyright (c) 2014 Constantine Mureev. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
